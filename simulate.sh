00b46e820924461437bd0f7c75c8d0e63c330308
lusciouspear <taylor69>
2016-02-03 12:31:05 -0800

    remove cruft

--- simulate.sh
+++ simulate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-mvn package
 wget -N https://s3.amazonaws.com/com.22acacia/releases/sossity/sossity/sossity-0.1.0-SNAPSHOT-standalone.jar
 java -jar sossity-0.1.0-SNAPSHOT-standalone.jar -c $1 -s -d
