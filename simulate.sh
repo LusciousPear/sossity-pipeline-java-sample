cfd5783d8684e783a741ab89edac4deee4c4bed6
lusciouspear <bradfordstephens@gmail.com>
2016-02-04 01:47:50 -0800

    fix deps

--- simulate.sh
+++ simulate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
-wget -N https://s3.amazonaws.com/com.22acacia/releases/sossity/sossity/sossity-0.1.0-SNAPSHOT-standalone.jar
+wget -N https://storage.googleapis.com/build-artifacts-public-eu/sossity/sossity-0.1.0-SNAPSHOT-standalone.jar
 java -jar sossity-0.1.0-SNAPSHOT-standalone.jar -c $1 -s -d
